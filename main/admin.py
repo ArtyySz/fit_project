76c01b8e166fee4f4c932b6e5d5df88a9acccc80
Arty <artemgoncharov23>
2026-04-02 20:15:22 +0300

    конфигурация администратора для упражнений, тренировок и тренировочных данных

--- main/admin.py
+++ main/admin.py
@@ -1,13 +1,80 @@
 from django.contrib import admin
-from main.models import MuscleGroup, Exercise, Workout, WorkoutHistory, PlanDay, WorkoutExercise, FavoriteExercise, Profile, TempUser
-# Register your models here.
+from .models import (
+    MuscleGroup,
+    Exercise,
+    Workout,
+    WorkoutHistory,
+    PlanDay,
+    WorkoutExercise,
+    FavoriteExercise,
+    Profile
+)
 
-admin.site.register(MuscleGroup)
-admin.site.register(Exercise)
-admin.site.register(Workout)
-admin.site.register(WorkoutHistory)
-admin.site.register(PlanDay)
-admin.site.register(WorkoutExercise)
-admin.site.register(FavoriteExercise)
-admin.site.register(Profile)
-admin.site.register(TempUser)
+class WorkoutExerciseInline(admin.TabularInline):
+    model = WorkoutExercise
+    extra = 1
+
+
+@admin.register(MuscleGroup)
+class MuscleGroupAdmin(admin.ModelAdmin):
+    list_display = ("id", "name")
+    search_fields = ("name",)
+
+
+@admin.register(Exercise)
+class ExerciseAdmin(admin.ModelAdmin):
+    list_display = ("id", "name", "difficulty")
+    list_filter = ("difficulty", "muscle_groups")
+    search_fields = ("name", "description", "equipment", "execution_steps")
+    filter_horizontal = ("muscle_groups",)
+
+
+@admin.register(Workout)
+class WorkoutAdmin(admin.ModelAdmin):
+    list_display = ("id", "name", "creator", "is_premade", "created_at")
+    list_filter = ("is_premade", "created_at")
+    search_fields = ("name", "description", "creator__username")
+    inlines = [WorkoutExerciseInline]
+
+
+@admin.register(WorkoutExercise)
+class WorkoutExerciseAdmin(admin.ModelAdmin):
+    list_display = ("id", "workout", "exercise", "sets", "reps", "rest_seconds", "order")
+    list_filter = ("workout",)
+    search_fields = ("workout__name", "exercise__name")
+
+
+@admin.register(WorkoutHistory)
+class WorkoutHistoryAdmin(admin.ModelAdmin):
+    list_display = ("id", "user", "workout", "duration_minutes", "completed_at")
+    list_filter = ("completed_at",)
+    search_fields = ("user__username", "workout__name")
+
+
+@admin.register(PlanDay)
+class PlanDayAdmin(admin.ModelAdmin):
+    list_display = ("id", "user", "day_of_week", "workout", "muscle_group")
+    list_filter = ("day_of_week",)
+    search_fields = ("user__username", "workout__name")
+
+
+@admin.register(FavoriteExercise)
+class FavoriteExerciseAdmin(admin.ModelAdmin):
+    list_display = ("id", "user", "exercise", "added_at")
+    list_filter = ("added_at",)
+    search_fields = ("user__username", "exercise__name")
+
+
+@admin.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = (
+        "user",
+        "full_name",
+        "age",
+        "weight",
+        "height",
+        "total_workouts",
+        "total_exercises_done",
+        "last_workout_date",
+    )
+    search_fields = ("user__username", "full_name")
